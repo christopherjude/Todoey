49fefb2685a0e0cf57249196fb0c3fd762d0fd6f
Chris Jude <therealmessenger@gmail.com>
2019-05-09 20:36:25 +0630

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Christopher Jude on 5/8/19.
+//  Copyright © 2019 Christopher Jude. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
